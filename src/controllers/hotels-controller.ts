3fcd941f4561b1b1e671d30f812bd52ad329b631
lucasmartins-dev <lucasmartinsms.dev@gmail.com>
2023-04-24 01:09:57 -0300

    feat: get hotels controller

---
+++ src/controllers/hotels-controller.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Response } from 'express';
+import httpStatus from 'http-status';
+import { AuthenticatedRequest } from '@/middlewares';
+import hotelsService from '@/services/hotels-service';
+import { notFoundError } from '@/errors';
+
+export async function getHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { userId } = req;
+  try {
+    const hotels = await hotelsService.getHotels(userId);
+    if (hotels.length == 0) throw notFoundError();
+    res.status(httpStatus.OK).send(hotels);
+  } catch (error) {
+    next(error);
+  }
+}
